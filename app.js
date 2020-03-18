abd26bd227aef54151bc1b7c3461f4b43def4b97
britt <britt110100@gmail.com>
2020-03-18 19:06:17 +0100

    adding random soup from meal api
    next = getting random soup with cold weather, and salad with warm weather
    
    adding image in .html

--- app.js
+++ app.js
@@ -3,18 +3,25 @@ class App {
         this.getLocation();
         this.lat;
         this.lng;
+ 
     }
+
+  
+
     getLocation(){
         navigator.geolocation.getCurrentPosition(
             this.gotLocation.bind(this), 
             this.errorLocation.bind(this)
         );  
     }
+
     gotLocation(result){
         this.lat = result.coords.latitude;
         this.lng = result.coords.longitude;
         this.getWeather();
+        this.getMeal();
     }
+
 
     getWeather(){
         let url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/1b4646d678ca3d602e91ca76a3aee6b9/${this.lat},${this.lng}?units=si`;
@@ -33,6 +40,25 @@ class App {
     errorLocation(err){
         console.log(err);
     }
+
+
+
+
+    getMeal(){
+        let url = `https://cors-anywhere.herokuapp.com/https://api.edamam.com/search?q=soup&app_id=8759285f&app_key=f5a5b7640e2000c5b8e7939c82341ea9`;
+        fetch(url).then(mealResponse => {
+            return mealResponse.json();
+        }).then(mealData => {
+            console.log(mealData);
+            console.log(Math.round(Math.random() * 10))
+            document.querySelector("#meal").innerHTML = 
+                mealData.hits[Math.round(Math.random() * 10)].recipe.label;
+        }).catch(err => {
+            console.log(err);
+        })
+    }
+
+    
 }
 
 let app = new App();
